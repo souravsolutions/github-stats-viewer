eced397e38db1d86515b543bcba89bd555300e21
souravsolutions <souravsardar836@gmail.com>
2026-03-08 04:09:48 +0530

    feat(analytics): add vercel analytics tracking

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Router from "./router.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <Router />
+    <Analytics />
   </QueryClientProvider>,
 );
